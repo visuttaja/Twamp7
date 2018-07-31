cc89a4824919a7705e3afc644133f61a5d1129f4
Visu <visuttaja@gmail.com>
2018-07-31 05:13:37 +0300

    Remove All refine

--- Remove_Installation.bat
+++ Remove_Installation.bat
@@ -6,13 +6,13 @@ echo Tiedosto:  %~dp0%~n0%~x0
 echo älämölö
 call set_roots.bat
 call set_mysql_environment.bat
-echo mysql_data_dir %mysql_data_dir%
-echo %mysql_data_dir%
-rmdir "%mysql_root%\%mysql_versions_dir%\%mysql_current_version%  /S /Q
-rem rmdir "%mysql_data_dir%" /S /Q
-rem rmdir /s "%siteroot%/htdocs/wordpress" /S /Q
+
+
 rmdir %apache_root%  /S /Q
 rmdir %php_root%  /S /Q
+rmdir "%mysql_root%\%mysql_versions_dir%\%mysql_current_version%"  /S /Q
+
+rem rmdir /s "%siteroot%/htdocs/wordpress" /S /Q
 
 pause
 popd
